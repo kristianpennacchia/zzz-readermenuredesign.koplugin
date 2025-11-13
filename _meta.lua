7c4e7f0d275c30a1c086a0c64231fa2ceaa11024
Kristian Pennacchia <clubsport7@hotmail.com>
2025-11-13 17:08:32 +1100

    Changed version to “v1.0.5”.

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
 	name = "zzz-readermenuredesign",
 	fullname = _("Reader Menu Redesign"),
 	description = _([[A redesign of the various reader menus in KOReader.]]),
-	version = "v1.0.4",
+	version = "v1.0.5",
 }
